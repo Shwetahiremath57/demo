21b03aa49add1b9380278102e2b6d11ba94fe194
Shwetahiremath57 <shwetahiremath17xworkz@gmail.com>
2021-12-20 17:04:37 +0530

    added : demo java file

---
+++ makeup.java
@@ -0,0 +1,28 @@
+class makeup{
+                    public static void main(String[] args){
+                    byte primer=56;
+                    System.out.println(primer);
+
+                    int foudation=126;
+                    System.out.println(foudation);
+
+                    short concealer=260;
+                    System.out.println(concealer );
+
+                    long compact=230;
+                    System.out.println(compact);
+
+                    float kajal=162.20f;
+                    System.out.println(kajal);
+
+                    double eyelinear=299.99999;
+                    System.out.println(eyelinear);
+                    
+                    boolean liplinearred=true;
+                    System.out.println(liplinearred);
+                    
+                    char lipstick='p';
+                    System.out.println(lipstick);
+                    
+                    }
+}
